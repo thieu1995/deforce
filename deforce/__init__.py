16afcde5c91ff6fe11bbfe55514d088ed8da758b
Thieu Nguyen <nguyenthieu2102@gmail.com>
2024-04-05 15:10:03 +0700

    Update models

---
+++ deforce/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 10/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "0.0.1"
+
+from deforce.helpers.preprocessor import Data, DataTransformer
+from deforce.core.traditional_cfn import CfnRegressor, CfnClassifier
+from deforce.core.dfo_cfn import DfoCfnRegressor, DfoCfnClassifier
